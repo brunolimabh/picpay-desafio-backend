82e35d4f72341c50aa5cad878f089c366df2fb12
Bruno Henrique <bruno.lima@sptech.school>
2024-05-06 13:47:43 -0300

    feat: create UserRepository

---
+++ src/main/java/com/example/picpaydesafiobackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.picpaydesafiobackend.repository;
+
+import com.example.picpaydesafiobackend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+}
